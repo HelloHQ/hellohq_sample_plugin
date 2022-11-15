4af6881ac1b541f617c22f86b6c92d0f78b795a6
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2022-11-15 17:37:06 +0700

    create react vite UI

---
+++ ui/src/main.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+import Plugin from './Plugin'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('plugin')).render(
+  <React.StrictMode>
+    <Plugin />
+  </React.StrictMode>
+)
