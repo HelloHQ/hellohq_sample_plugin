b01070461a25f4e4c0a695f8eedf30a125cedb0d
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2022-11-15 21:37:18 +0700

    Create lit component

---
+++ ui/simple.js
@@ -0,0 +1,23 @@
+import { LitElement, css, html } from "lit";
+
+export class SimpleGreeting extends LitElement {
+  static styles = css`
+    p {
+      color: blue;
+    }
+  `;
+
+  static properties = {
+    name: { type: String },
+  };
+
+  constructor() {
+    super();
+    this.name = "Somebody";
+  }
+
+  render() {
+    return html`<p>Hello, ${this.name}!</p>`;
+  }
+}
+customElements.define("simple-greeting", SimpleGreeting);
